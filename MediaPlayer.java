650daebd8a474fe6cbdb0dd716716df72e85749c
girishalwani <alwanigirish82@gmail.com>
2019-12-06 02:13:13 +0530

    Add files via upload
    Media Player application using JAVA and scene builder ( for UI ).

---
+++ MediaPlayer.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mediaplayer;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.media.Media;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author user
+ */
+public class MediaPlayer extends Application {
+
+    
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        stage.setTitle("Babloo Media");
+        
+        scene.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent doubleClicked) {
+                if(doubleClicked.getClickCount()==2){
+                    stage.setFullScreen(true);
+                }
+                if(stage.isFullScreen()==true && doubleClicked.getClickCount()==2){
+                    stage.setScene(scene);
+                }
+                    
+            }
+        });
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
